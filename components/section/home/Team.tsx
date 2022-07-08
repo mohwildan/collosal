de4f79794d01fd1387f42a2862112ac50707f5fa
mohwildan <mobafreewtf@gmail.com>
2022-07-08 16:50:49 +0700

    fix bug

--- components/section/home/Team.tsx
+++ components/section/home/Team.tsx
@@ -9,7 +9,7 @@ const Team = () => {
         <Box flex={1} mt={16}>
           <Heading variant="Section-Badge">OUR TEAM</Heading>
           <Heading variant="Section-Title" mt={4}>
-            We're a team of designers, engineers and analysts
+            We&apos;re a team of designers, engineers and analysts
           </Heading>
           <Text variant="Section-Paragraph" mt={6}>
             Our team consists of many creative people. We are committed to
